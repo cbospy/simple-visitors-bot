8712cb7d4fe2efd6b4e749f3f5616fbf209e4e02
cbosfaitdesprout <108202522+cbospy@users.noreply.github.com>
2022-09-01 20:53:50 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+
+chrome = webdriver.Chrome(executable_path="chromedriver")
+chrome1 = webdriver.Chrome(executable_path="chromedriver")
+chrome2 = webdriver.Chrome(executable_path="chromedriver")
+chrome3 = webdriver.Chrome(executable_path="chromedriver")
+chrome4 = webdriver.Chrome(executable_path="chromedriver")
+chrome5 = webdriver.Chrome(executable_path="chromedriver")
+chrome6 = webdriver.Chrome(executable_path="chromedriver")
+chrome7 = webdriver.Chrome(executable_path="chromedriver")
+chrome8 = webdriver.Chrome(executable_path="chromedriver")
+chrome9 = webdriver.Chrome(executable_path="chromedriver")
+
+
+
+chrome.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome1.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome2.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome3.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome4.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome5.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome6.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome7.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome8.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+chrome9.get("https://camo.githubusercontent.com/aebab50996a562efb9f0dd99a01f498fdd4465da5fda1d8d3ff6fc9f89a39974/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f63626f7370792f636f756e742e737667")
+
+while True:
+    chrome.refresh()
+    chrome1.refresh()
+    chrome2.refresh()
+    chrome3.refresh()
+    chrome4.refresh()
+    chrome5.refresh()
+    chrome6.refresh()
+    chrome7.refresh()
+    chrome8.refresh()
+    chrome9.refresh()
+    time.sleep(1)
